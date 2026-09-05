b0041b496ad561ddc37eb6a45e7c400cf2930d69
Zaara0206 <zaaraiqbal02@gmail.com>
2026-09-06 02:31:34 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            // Try selling today
+            int profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit, profit);
+
+            // Keep the cheapest buying price
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
